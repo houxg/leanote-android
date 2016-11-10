7f9f29f2e79d698b77b335a4a53b4a07556e6ac0
houxg <houxg09@foxmail.com>
2016-11-10 14:56:42 +0800

    add usn property to match response

--- app/src/main/java/com/leanote/android/model/UpdateRet.java
+++ app/src/main/java/com/leanote/android/model/UpdateRet.java
@@ -1,7 +1,14 @@
 package com.leanote.android.model;
 
 
+import com.google.gson.annotations.SerializedName;
 import com.leanote.android.networking.retrofitapi.model.BaseResponse;
 
 public class UpdateRet extends BaseResponse {
+    @SerializedName("Usn")
+    int usn;
+
+    public int getUsn() {
+        return usn;
+    }
 }
